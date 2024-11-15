d007114b0fc14288ac35c4c4a7c062be38787a5d
Boutzi <joe@arkaans.com>
2024-11-15 21:05:09 +0100

    Delete tests directory

--- tests/niveau-1/calculatrice.py
+++
@@ -1,7 +0,0 @@
-# Initialisation de la variable "resultat" à 0
-resultat = 0
-
-# Point d'entrée principal de l'application
-if __name__ == "__main__":
-    # Affiche le résultat (actuellement 0) dans la console
-    print(resultat)
